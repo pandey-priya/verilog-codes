79f3bd67231d8e757903cfafb093951724cf5a35
Priya Pandey <118142904+priyapandey-github@users.noreply.github.com>
2023-07-23 19:59:17 -0400

    Update vending-machine-fsm.v
    In this wending machine, it accepts only two coins, 5 point and 10 point. Whenever total of coins equal to 15 points, then nw_pa signal will go high and user will get news paper. It will not return any coin, if total of points exceeds 15 points.

--- vending-machine-fsm.v
+++ vending-machine-fsm.v
@@ -22,61 +22,49 @@ module vending_machine(nw_pa,clk,coin,rst);
      end
 
 
-   always @(state,coin)
-     begin
-    case (state)
-     s0:
-        begin
-           if (coin==2'b00)
-        next_state=s0;
-           else
-       if (coin==2'b01)
-          next_state=s5;
-       else
-         if (coin==2'b10)
-          next_state=s10;
-        end
-       s5:
-          begin
-       if (coin==2'b00)
-          next_state=s5;
-       else
-         if (coin==2'b01)
-            next_state=s10;
-         else
-           if (coin==2'b10)
-             next_state=s15;
-             end
-       s10:
-          begin
-           if (coin==2'b00)
-        next_state=s10;
-           else
-       if (coin==2'b01)
-          next_state=s15;
-       else
-        if (coin==2'b10)
-            next_state=s15;  
-           end
-       s15:
-          begin
-       next_state=s0;
-          end
-       default : next_state=s0;
-   
-    endcase // case (state)
-     end // always @ (state,next_state)
+   always @(state,coin) begin
+      case (state)
+         s0: begin
+            if (coin==2'b00)
+               next_state=s0;
+            else if (coin==2'b01)
+               next_state=s5;
+            else if (coin==2'b10)
+               next_state=s10;
+         end
+         s5: begin
+            if (coin==2'b00)
+               next_state=s5;
+            else if (coin==2'b01)
+               next_state=s10;
+            else if (coin==2'b10)
+               next_state=s15;
+         end
+         s10: begin
+            if (coin==2'b00)
+               next_state=s10;
+            else if (coin==2'b01)
+               next_state=s15;
+            else if (coin==2'b10)
+               next_state=s15;  
+         end
+         s15: begin
+            next_state=s0;
+         end
+            
+         default : next_state=s0;
+      endcase // case (state)
+   end // always @ (state,next_state)
 
 
-   always @(state)
- begin
-   case (state)
+   always @(state) begin
+      case (state)
       s0 : nw_pa<=1'b0;
       s5 : nw_pa<=1'b0;
       s10: nw_pa<=1'b0;
       s15: nw_pa<=1'b1;
-    default: nw_pa<=1'b0;
-   endcase // case (state)
- end
- 
+      default: nw_pa<=1'b0;
+      endcase // case (state)
+   end
+   
 endmodule
