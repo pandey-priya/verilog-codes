c4582063672108b71042301d050a1f7d3bd2a94f
Priya Pandey <118142904+pandey-priya@users.noreply.github.com>
2023-08-04 09:48:26 -0700

    Create 4bit-counter.v

---
+++ COUNTER/4bit-counter.v
@@ -0,0 +1,15 @@
+module counter(Q , clock, clear);
+
+output [3:0] Q;
+input clock, clear;
+//output defined as register
+reg [3:0] Q;
+
+always @( posedge clear  or negedge clock)
+begin
+       if (clear)
+          Q = 4'd0;
+       else
+          Q = (Q + 1) ;
+end
+endmodule
