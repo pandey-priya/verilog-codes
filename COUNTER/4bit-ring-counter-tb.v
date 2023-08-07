2355064164105a5bdb1d1f121e357d126f3d08c9
Priya Pandey <118142904+pandey-priya@users.noreply.github.com>
2023-08-07 15:04:34 -0700

    Create 4bit-ring-counter-tb.v

---
+++ COUNTER/4bit-ring-counter-tb.v
@@ -0,0 +1,36 @@
+module tb_ring;
+
+    // Inputs
+    reg Clock;
+    reg Reset;
+
+    // Outputs
+    wire [3:0] Count_out;
+  
+  	initial begin
+      $dumpfile("tb_ring.vcd");
+      $dumpvars(0,tb_ring);
+    end
+
+    // Instantiate the Unit Under Test (UUT)
+    ring_counter uut (
+        .Clock(Clock), 
+        .Reset(Reset), 
+        .Count_out(Count_out)
+    );
+
+    ///////////////////Clock generation ///////////////////////////
+    initial Clock = 0; 
+    always #10 Clock = ~Clock; 
+    ////////// #10 means wait for 10 ns before executing the next statement. ///////////
+    
+    //Simulation inputs.
+    initial begin
+    //Apply Reset for 50 ns.
+        Reset = 1; //Reset is high
+        #50;       //Wait for 50 ns
+        Reset = 0; //Reset is low.
+      	#500 $stop;
+    end
+      
+endmodule
